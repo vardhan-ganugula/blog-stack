df2140c49605825c02540e08e272255a2995eef5
vardhan ganugula <gvardhan2004@gmail.com>
2025-05-30 12:37:14 +0530

    fix blog post width and padding

--- client/src/pages/BlogPost.jsx
+++ client/src/pages/BlogPost.jsx
@@ -19,13 +19,13 @@ const BlogPost = () => {
             <h1 className='blog__title'>{blog.title}</h1>
             <img src={blog.imageUrl} alt='testing' className='blog_feature_image' />
             <div className='blog__content'>
-              <div dangerouslySetInnerHTML={{__html: blog.content}}>
+              <div className='blog__content__inner' dangerouslySetInnerHTML={{__html: blog.content}}>
 
               </div>
             </div>
             <div className='blog__tags'>
               {
-                blog.tags.length > 0 && blog.tags.map((tag) => <span className='blog__tag'>{tag}</span>)
+                blog.tags.length > 0 && blog.tags.map((tag, idx) => <span key={idx} className='blog__tag'>{tag}</span>)
               }
             </div>
           </article>
